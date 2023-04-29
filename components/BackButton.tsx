501a11d0560d02e3e0e2aa42c259e80f6cc07ef9
matthewemeh <mcmekus10@gmail.com>
2023-04-29 19:33:07 +0100

    added back button

---
+++ components/BackButton.tsx
@@ -0,0 +1,21 @@
+import { ExtraStyle } from '@/public/interfaces';
+
+interface Props {
+  onClick?: () => void;
+  extraStyles?: ExtraStyle;
+}
+
+const BackButton: React.FC<Props> = ({ onClick, extraStyles }) => {
+  return (
+    <button
+      onClick={() => {
+        window.history.back();
+        if (onClick) onClick();
+      }}
+      style={extraStyles}
+      className='absolute z-10 hidden top-[54px] left-[15px] bg-[url(/assets/svgs/caret-left.svg)] bg-center bg-no-repeat w-[30px] h-[30px] rounded-[60px] bg-athens-gray phones:block'
+    />
+  );
+};
+
+export default BackButton;
