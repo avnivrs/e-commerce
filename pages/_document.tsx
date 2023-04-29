aa7abfcef10dfcfa775506a8ff29e15a1570feec
matthewemeh <mcmekus10@gmail.com>
2023-04-29 07:14:13 +0100

    removed redundant font fetch api

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,13 +17,6 @@ export default function Document() {
         <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
-
-        {/* <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap'
-        /> */}
       </Head>
       <body className='text-outer-space bg-white dark:text-wild-sand dark:bg-shark'>
         <Main />
